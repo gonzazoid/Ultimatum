1dc428515d8b802192359bafb5e3ccd7f6bf6b94
Appaev Timur <workstation@Appaevs-MacBook-Air.local>
2021-12-13 00:06:01 +0300

    - URLRequest::AnnotateAndMoveUserBlockedCookies

--- net/url_request/url_request.h
+++ net/url_request/url_request.h
@@ -894,9 +894,9 @@ class NET_EXPORT URLRequest : public base::SupportsUserData {
   // These functions delegate to the NetworkDelegate if it is not nullptr.
   // Otherwise, cookies can be used unless SetDefaultCookiePolicyToBlock() has
   // been called.
-  void AnnotateAndMoveUserBlockedCookies(
-      CookieAccessResultList& maybe_included_cookies,
-      CookieAccessResultList& excluded_cookies) const;
+  // void AnnotateAndMoveUserBlockedCookies(
+  //     CookieAccessResultList& maybe_included_cookies,
+  //     CookieAccessResultList& excluded_cookies) const;
 //   bool CanSetCookie(const net::CanonicalCookie& cookie,
 //                     CookieOptions* options) const;
   PrivacyMode DeterminePrivacyMode() const;
--- net/url_request/url_request_job.cc
+++ net/url_request/url_request_job.cc
@@ -404,12 +404,12 @@ void URLRequestJob::NotifySSLCertificateError(int net_error,
                                               bool fatal) {
   request_->NotifySSLCertificateError(net_error, ssl_info, fatal);
 }
-
+// XXX remove
 void URLRequestJob::AnnotateAndMoveUserBlockedCookies(
     CookieAccessResultList& maybe_included_cookies,
     CookieAccessResultList& excluded_cookies) const {
-  request_->AnnotateAndMoveUserBlockedCookies(maybe_included_cookies,
-                                              excluded_cookies);
+  // request_->AnnotateAndMoveUserBlockedCookies(maybe_included_cookies,
+  //                                             excluded_cookies);
 }
 
 // bool URLRequestJob::CanSetCookie(const net::CanonicalCookie& cookie,
