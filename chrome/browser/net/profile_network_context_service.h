7aa85ed9161d8436b760f3155b4b047d8341c1ae
Timur Appaev <gonzazoid@gonzazoid.com>
2021-12-18 02:27:06 +0300

    - OnThirdPartyCookieBlockingChanged

--- chrome/browser/net/profile_network_context_service.h
+++ chrome/browser/net/profile_network_context_service.h
@@ -163,8 +163,8 @@ class ProfileNetworkContextService
   //                              ContentSettingsType content_type) override;
 
   // content_settings::CookieSettings::Observer:
-  void OnThirdPartyCookieBlockingChanged(
-      bool block_third_party_cookies) override;
+  // void OnThirdPartyCookieBlockingChanged(
+  //     bool block_third_party_cookies) override;
 
   Profile* const profile_;
 
