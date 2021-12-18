a9272f473a0c8bed4604d0dbeb88e2b693391c6c
Timur Appaev <gonzazoid@gonzazoid.com>
2021-12-18 22:00:59 +0300

    cookie_monster: deflating

--- net/cookies/cookie_util.cc
+++ net/cookies/cookie_util.cc
@@ -245,10 +245,10 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContextForSet(
   return MakeSameSiteCookieContext(result, schemeful_result);
 }
 
-bool CookieWithAccessResultSorter(const CookieWithAccessResult& a,
-                                  const CookieWithAccessResult& b) {
-  return CookieMonster::CookieSorter(&a.cookie, &b.cookie);
-}
+// bool CookieWithAccessResultSorter(const CookieWithAccessResult& a,
+//                                   const CookieWithAccessResult& b) {
+//   return CookieMonster::CookieSorter(&a.cookie, &b.cookie);
+// }
 
 }  // namespace
 
@@ -848,8 +848,8 @@ NET_EXPORT void DCheckIncludedAndExcludedCookieLists(
                                }));
 
   // Check that the included cookies are still in the correct order.
-  DCHECK(
-      base::ranges::is_sorted(included_cookies, CookieWithAccessResultSorter));
+  // DCHECK(
+  //     base::ranges::is_sorted(included_cookies, CookieWithAccessResultSorter));
 }
 
 }  // namespace cookie_util
