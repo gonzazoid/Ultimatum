437a727da0d55cd0b386cdb7d830e339137376b0
Timur Appaev <gonzazoid@gonzazoid.com>
2021-12-18 14:37:18 +0300

    - GetAllCookiesAsync

--- net/cookies/cookie_monster.h
+++ net/cookies/cookie_monster.h
@@ -183,7 +183,7 @@ class NET_EXPORT CookieMonster : public CookieStore {
   //                                    const CookieOptions& options,
   //                                    const CookiePartitionKeyCollection& s,
   //                                    GetCookieListCallback callback) override;
-  void GetAllCookiesAsync(GetAllCookiesCallback callback) override;
+  // void GetAllCookiesAsync(GetAllCookiesCallback callback) override;
   void GetAllCookiesWithAccessSemanticsAsync(
       GetAllCookiesWithAccessSemanticsCallback callback) override;
   void DeleteCanonicalCookieAsync(const CanonicalCookie& cookie,
