ba318657f987e3b0e847a763adb3aebaea4579de
Timur Appaev <gonzazoid@gonzazoid.com>
2021-12-19 18:51:02 +0300

    - cookieable_schemes_

--- net/cookies/cookie_monster.h
+++ net/cookies/cookie_monster.h
@@ -745,7 +745,7 @@ class NET_EXPORT CookieMonster : public CookieStore {
   // wanted.  Thus this value is not initialized.
   // base::Time earliest_access_time_;
 
-  std::vector<std::string> cookieable_schemes_;
+  // std::vector<std::string> cookieable_schemes_;
 
   // base::Time last_statistic_record_time_;
 
