76dacdc0644c3ea076f5d58c7553b6c41365bff9
gonzazoid <alloaolla@gmail.com>
2025-11-21 16:36:55 +0300

    external downloader option

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/settings/DownloadSettings.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/settings/DownloadSettings.java
@@ -4,10 +4,26 @@
 
 package org.chromium.chrome.browser.download.settings;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import java.util.HexFormat;
+import java.io.UnsupportedEncodingException;
 
 import androidx.preference.Preference;
 
+import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.build.annotations.NullMarked;
@@ -26,6 +42,8 @@ import org.chromium.components.browser_ui.settings.ManagedPreferenceDelegate;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.components.user_prefs.UserPrefs;
 
+// import org.chromium.components.browser_ui.settings.ChromeBaseCheckBoxPreference;
+
 /** Fragment containing Download settings. */
 @NullMarked
 public class DownloadSettings extends ChromeBaseSettingsFragment
@@ -39,6 +57,8 @@ public class DownloadSettings extends ChromeBaseSettingsFragment
     private ManagedPreferenceDelegate mLocationPromptEnabledPrefDelegate;
     private ChromeSwitchPreference mAutoOpenPdfEnabledPref;
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
+
+    private ChromeSwitchPreference mExternalDownloadManager;
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String s) {
@@ -80,6 +100,9 @@ public class DownloadSettings extends ChromeBaseSettingsFragment
                             : getActivity().getString(R.string.auto_open_pdf_enabled_description);
             mAutoOpenPdfEnabledPref.setSummaryOn(summary);
         }
+
+        mExternalDownloadManager = (ChromeSwitchPreference) findPreference("enable_external_download_manager");
+        mExternalDownloadManager.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -126,6 +149,28 @@ public class DownloadSettings extends ChromeBaseSettingsFragment
                     UserPrefs.get(getProfile()).getBoolean(Pref.AUTO_OPEN_PDF_ENABLED));
             mAutoOpenPdfEnabledPref.setEnabled(true);
         }
+
+        if (mExternalDownloadManager != null) {
+            mExternalDownloadManager.setChecked(ContextUtils.getAppSharedPreferences().getBoolean("enable_external_download_manager", false));
+            if (ContextUtils.getAppSharedPreferences().getBoolean("enable_external_download_manager", false)
+                  && !TextUtils.isEmpty(ContextUtils.getAppSharedPreferences().getString("selected_external_download_manager_package_name", ""))) {
+                mExternalDownloadManager.setSummary(ContextUtils.getAppSharedPreferences().getString("selected_external_download_manager_package_name", ""));
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == 4242 && resultCode == Activity.RESULT_OK && data != null) {
+                 ComponentName componentName = data.getComponent();
+                 final String packageName = componentName.getPackageName();
+                 final String activityName = componentName.getClassName();
+                 SharedPreferences.Editor sharedPreferencesEditor = ContextUtils.getAppSharedPreferences().edit();
+                 sharedPreferencesEditor.putString("selected_external_download_manager_package_name", packageName);
+                 sharedPreferencesEditor.putString("selected_external_download_manager_activity_name", activityName);
+                 sharedPreferencesEditor.apply();
+                 updateDownloadSettings();
+            }
     }
 
     // Preference.OnPreferenceChangeListener implementation.
@@ -145,6 +190,37 @@ public class DownloadSettings extends ChromeBaseSettingsFragment
             }
         } else if (PREF_AUTO_OPEN_PDF_ENABLED.equals(preference.getKey())) {
             UserPrefs.get(getProfile()).setBoolean(Pref.AUTO_OPEN_PDF_ENABLED, (boolean) newValue);
+        } else if ("enable_external_download_manager".equals(preference.getKey())) {
+            SharedPreferences.Editor sharedPreferencesEditor = ContextUtils.getAppSharedPreferences().edit();
+            sharedPreferencesEditor.putBoolean("enable_external_download_manager", (boolean)newValue);
+            sharedPreferencesEditor.apply();
+            if ((boolean)newValue == true) {
+                    // List<Intent> targetedShareIntents = new ArrayList<Intent>();
+                    Intent shareIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://test.com/file.rar"));
+                    // Set title and text to share when the user selects an option.
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, "http://test.com/file.rar");
+                    List<ResolveInfo> resInfo = getActivity().getPackageManager().queryIntentActivities(shareIntent, 0);
+                    if (!resInfo.isEmpty()) {
+                        // String packageName = ContextUtils.getApplicationContext().getPackageName();
+                        // for (ResolveInfo info : resInfo) {
+                        //     if (!packageName.equals(info.activityInfo.packageName)) {
+                        //         Intent targetedShare = new Intent(android.content.Intent.ACTION_VIEW);
+                        //         targetedShare.setPackage(info.activityInfo.packageName.toLowerCase(Locale.ROOT));
+                        //         targetedShareIntents.add(targetedShare);
+                        //     }
+                        // }
+                        // Then show the ACTION_PICK_ACTIVITY to let the user select it
+                        Intent intentPick = new Intent();
+                        intentPick.setAction(Intent.ACTION_PICK_ACTIVITY);
+                        // Set the title of the dialog
+                        intentPick.putExtra(Intent.EXTRA_TITLE, "Select download manager");
+                        intentPick.putExtra(Intent.EXTRA_INTENT, shareIntent);
+                        // intentPick.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray());
+
+                        // Call StartActivityForResult so we can get the app name selected by the user
+                        this.startActivityForResult(intentPick, /* REQUEST_CODE_MY_PICK */ 4242);
+                    }
+            }
         }
         return true;
     }
